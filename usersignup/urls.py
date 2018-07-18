959ba2f67ab9f214af352a99c1361e25a9d917b1
tuff <tuffjunkmail@gmail.com>
2018-07-17 22:23:47 -0700

    New User signup app

--- usersignup/urls.py
+++ usersignup/urls.py
@@ -3,5 +3,5 @@ from usersignup import views
 
 app_name='usersignup'
 urlpatterns = [
-    path('usersignup/', views.usersignup, name='usersignup'),
+    path('', views.usersignup, name='usersignup'),
 ]
