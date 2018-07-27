1b1194448c4a338d0a6539118644810ab0e39acc
Ubuntu <ubuntu@ip-172-31-13-128.us-west-1.compute.internal>
2018-07-27 00:23:18 +0000

    no venv files

--- cquser/models.py
+++ cquser/models.py
@@ -5,4 +5,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
+
+
+class user_token(models.Model):
+    access_token=models.CharField(max_length=80)
+    refresh_token=models.CharField(max_length=80)
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=True)
+
+    
+    def __str__(self):
+        return self.access_token
+    def __str__(self):
+        return self.refresh_token
